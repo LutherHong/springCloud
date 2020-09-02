61f3a4014fb3b3465d9d51be9c450528de47289a
zhaozhihong <zhaozhihong@inspur.com>
2020-09-02 21:26:53 +0800

    feat： Zuul网关

--- product-view-service-feign/src/main/java/org/example/client/ProductClientFeign.java
+++ product-view-service-feign/src/main/java/org/example/client/ProductClientFeign.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(value = "PRODUCT-DATA-SERVICE")
+//@FeignClient(value = "PRODUCT-DATA-SERVICE")
+// 如果访问的 PRODUCT-DATA-SERVICE 不可用的话，就调用 ProductClientFeignHystrix 来进行反馈信息。
+@FeignClient(value = "PRODUCT-DATA-SERVICE",fallback = ProductClientFeignHystrix.class)
 public interface ProductClientFeign {
 
     @GetMapping("/products")
